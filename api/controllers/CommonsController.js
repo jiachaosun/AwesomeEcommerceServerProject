a82b95d15aebfdbcc9350e1bba3b3cdac1550be2
jiachaosun <jiachaosun@gmail.com>
2017-02-07 10:23:41 +0800

    add modals

---
+++ api/controllers/CommonsController.js
@@ -0,0 +1,11 @@
+/**
+ * CommonsController
+ *
+ * @description :: Server-side logic for managing commons
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
